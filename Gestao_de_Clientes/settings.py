e454f737e737ee1a4a150b600aa8a3080fd76704
Jorge Junio <jorgejunioufrn@gmail.com>
2020-12-15 15:54:42 -0300

    Removida a pasta .idea do repositorio, pois só tem arquivos de configuração da ide

--- Gestao_de_Clientes/settings.py
+++ Gestao_de_Clientes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Gestao_de_Clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestao_clientes',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
